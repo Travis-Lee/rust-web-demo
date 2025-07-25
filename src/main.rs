bb3b127492e31b876a28540621f9ebd842d1f354
Travis-Lee <lixiang.fr@hotmail.com>
2025-07-25 17:34:19 +0000

    change title name

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ fn base_context() -> Context {
     // Header 相关
     context.insert("home_url", "/");
     context.insert("title", "Lee博客");
-    context.insert("header_title", "Lee的技术分享");
+    context.insert("header_title", "浩·仁");
     context.insert("logo_url", "/static/images/logo.png");
 
     context.insert("posts_url", "/posts");
