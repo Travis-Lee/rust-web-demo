699682a4964454f810b00cb44c36567dd34f92cc
Travis-Lee <lixiang.fr@hotmail.com>
2025-07-28 22:23:12 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ lazy_static! {
     static ref TERA: Tera = {
         let tera = match Tera::new("templates/**/*.html") {
             Ok(t) => {
+                println!("加载模板列表:");
+                for name in t.get_template_names() {
+                    println!(" - {}", name);
+                }
                 t
             }
             Err(e) => {
